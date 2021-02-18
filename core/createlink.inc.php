d013b37e2f4c440eb2b344490ae84b0788e654bb
Connor <connortews@connort.com>
2021-02-18 14:14:53 -0600

    1.1.8

--- core/createlink.inc.php
+++ core/createlink.inc.php
@@ -1,5 +1,6 @@
 <?php
-function linkgen() {
+function linkgen()
+{
   $config = require '../config.php';
   $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
   $pass = array();
@@ -10,6 +11,7 @@ function linkgen() {
   }
   return implode($pass);
 }
+$config = require '../config.php';
 if (!isset($_POST['long_link_sub'])) {
   header("Location: ../?error=0dm3fk");
   exit();
@@ -18,23 +20,64 @@ if (empty($_POST['long_link_inp'])) {
   header("Location: ../error=gkd0vv");
   exit();
 }
+session_start();
+if ($config['public_access'] === "false") {
+  header("Location: ../?error=vdclssc");
+  exit();
+}
 $longLink = $_POST['long_link_inp'];
-$longLink = htmlspecialchars($longLink);
-$longLink = stripslashes($longLink);
+// $longLink = htmlspecialchars($longLink);
+// $longLink = stripslashes($longLink);
 
-$counter = $_POST['click_count_check'];
+if (!empty($_POST['click_count_check'])) {
+  $counter = $_POST['click_count_check'];
+} else {
+  $counter = null;
+}
 
 $linkID = linkgen();
 
 $config = require '../config.php';
 
-if (!file_exists("../".$config['link_path'])) {
-  mkdir("../".$config['link_path']);
+if (!file_exists("../" . $config['link_path'])) {
+  mkdir("../" . $config['link_path']);
 }
 // if ($counter === "on") {
 
 // }
-if (!file_exists("../".$config['link_path']."/".$linkID."/")) {
-  mkdir("../".$config['link_path']."/".$linkID."/");
-  
+if (!file_exists("../" . $config['link_path'] . "/" . $linkID . "/")) {
+  mkdir("../" . $config['link_path'] . "/" . $linkID . "/");
+} else {
+  header("Location: ../error=dvmos0");
+  exit();
 }
+$index = "../" . $config['link_path'] . "/" . $linkID . "/index.php";
+$indexfile = fopen($index, "w") or die("Unable! vawWDAHD");
+$link = $config['domain_name'] . '/' . $config['link_path'] . '/' . $linkID;
+$indexcontent = '<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta http-equiv="refresh" content="0; url=' . $longLink . '/" />
+  </head>
+  <body></body>
+</html>';
+fwrite($indexfile, $indexcontent);
+fclose($indexfile);
+
+$data = "../" . $config['link_path'] . "/" . $linkID . "/data.php";
+$datafile = fopen($data, "w") or die("Unable! vawWDAHD");
+$link = $config['domain_name'] . '/' . $config['link_path'] . '/' . $linkID;
+$datacontent = "
+  <?php return [
+    'id' => '" . $linkID . "',
+    'long_link' => '" . $longLink . "',
+    'short_link' => '" . $link . "'
+  ];
+  ";
+fwrite($datafile, $datacontent);
+fclose($datafile);
+
+header("Location: ../?success");
+exit();
--- core/link_list.inc.php
+++ core/link_list.inc.php
@@ -9,27 +9,32 @@ foreach ($fileList as $filename) {
   echo  '
         <tr>
           <td>
-            <a href="https://connort.com/Loremipsumdolorsitametconsectetur">
-              <input type="text" name="longlink" id="longlink" value="https://connort.com/Loremipsumdolorsitametconsectetur" />
+            <a href="'.$metaData['long_link'].'">
+              <input type="text" name="longlink" id="longlink" value="'.$metaData['long_link'].'" />
             </a>
           </td>
           <td>
-            <a href="https://connort.com/d0v0am">
-              <input type="text" name="shortlink" id="shortlink" value="https://connort.com/d0v0am" />
+            <a href="'.$metaData['short_link'].'">
+              <input type="text" name="shortlink" id="shortlink_'.$metaData['id'].'" value="'.$metaData['short_link'].'/" />
             </a>
           </td>
-          <td class="clicks">
-            0
-          </td>
+          <td class="clicks">';
+            if (file_exists($metaLink."click.php")) {
+              // echo = $click['clicks'];
+              echo "PlaceHolder";
+            } else {
+              echo "0";
+            }
+          echo '</td>
           <td>
-            <button class="copy" type="button" class="copy" onclick="short_copy_REPLACE()" name="copy">
+            <button class="copy" type="button" class="copy" onclick="short_copy_'.$metaData['id'].'()" name="copy">
               <i data-feather="copy"></i>
             </button>
             <script>
-              function short_copy_REPLACE() {
-                var cpytxt = document.getElementById("shortlink");
-                cpytxt.select();
-                cpytxt.setSelectionRange(0, 99999);
+              function short_copy_'.$metaData['id'].'() {
+                var cpytxt_'.$metaData['id'].' = document.getElementById("shortlink_'.$metaData['id'].'");
+                cpytxt_'.$metaData['id'].'.select();
+                cpytxt_'.$metaData['id'].'.setSelectionRange(0, 99999)
                 document.execCommand("copy");
                 alert("Copied short url " + cpytxt.value);
               }
